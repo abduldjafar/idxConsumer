042cd881ad10f5f23c751330a55adccce7602b23
kotekaman <abdul.haris.djafar@gmail.com>
2022-05-16 11:21:23 +0700

    add self sertificate

--- src/main/java/idx/kafka/services/TopicProcessing.java
+++ src/main/java/idx/kafka/services/TopicProcessing.java
@@ -138,7 +138,7 @@ public class TopicProcessing {
                     System.out.println("file exists");
                     System.out.println(dataReturn.Filename);
                     String fileType = getFileType(dataReturn.Filename);
-                    sendFile(dataReturn.Filename,url,fileType,dataReturn.idxTotal.toString(),dataReturn.idxNumber.toString(),dataReturn.idxGroupId);
+                    //sendFile(dataReturn.Filename,url,fileType,dataReturn.idxTotal.toString(),dataReturn.idxNumber.toString(),dataReturn.idxGroupId);
                     // "http://localhost:8000/v1/idx/upload"
                 }
             }
